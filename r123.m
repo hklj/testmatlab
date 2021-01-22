9bbb5ecd346f3ea7f9f1cd2780b7064636e02ad3
liuxianghao <1078560865@qq.com>
2021-01-22 10:04:27 +0800

    add new

--- r123.m
+++ r123.m
@@ -2,3 +2,18 @@ clear
 clc
 t=1;
 v=t*2;
+K=100;p=5;r=0.22;
+s=0;
+for x=1:30
+    y=K*p*exp(r*x)./(K+p*(exp(r*x)-1));
+    y2=-x.*(x-30)/225;
+    s1=0.4*y*y2+0.2*y+30;
+    if s1>s
+        s=s1;
+        index=x;
+    end 
+
+end
+x=index;
+    y=K*p*exp(r*x)./(K+p*(exp(r*x)-1));
+    y2=-x.*(x-30)/225;
